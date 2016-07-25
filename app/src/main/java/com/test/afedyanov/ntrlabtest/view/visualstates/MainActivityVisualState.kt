59c4dece1a2dc66e0e840db580ce7f006583888c
alexandr.fedyanov <alexandr.fedyanov@retechlabs.com>
2016-07-25 21:00:49 +0700

    gitignore

--- app/src/main/java/com/test/afedyanov/ntrlabtest/view/visualstates/MainActivityVisualState.kt
+++ app/src/main/java/com/test/afedyanov/ntrlabtest/view/visualstates/MainActivityVisualState.kt
@@ -4,27 +4,28 @@ import android.view.View
 import com.test.afedyanov.ntrlabtest.view.activity.MainActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.view_data_item.*
 
 /**
  * Created by Alexandr on 25.07.2016.
  */
 
 fun MainActivity.setNormalState() {
-    rootLayout.visibility = View.VISIBLE
+    dataLayout.visibility = View.VISIBLE
     noDataTextView.visibility = View.INVISIBLE
     progress.visibility = View.INVISIBLE
     pullToRefresh.isRefreshing = false
 }
 
 fun MainActivity.setLoadingState() {
-    rootLayout.visibility = View.INVISIBLE
+    dataLayout.visibility = View.INVISIBLE
     noDataTextView.visibility = View.INVISIBLE
     progress.visibility = View.VISIBLE
     pullToRefresh.isRefreshing = false
 }
 
 fun MainActivity.setRefreshingState() {
-    rootLayout.visibility = View.VISIBLE
+    dataLayout.visibility = View.VISIBLE
     noDataTextView.visibility = View.INVISIBLE
     progress.visibility = View.INVISIBLE
     pullToRefresh.isRefreshing = true
@@ -36,7 +37,7 @@ fun MainActivity.setErrorState(errorMessage: String) {
 }
 
 fun MainActivity.setEmptyState() {
-    rootLayout.visibility = View.INVISIBLE
+    dataLayout.visibility = View.INVISIBLE
     noDataTextView.visibility = View.VISIBLE
     progress.visibility = View.INVISIBLE
     pullToRefresh.isRefreshing = false
